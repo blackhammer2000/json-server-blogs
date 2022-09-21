cab5ca376563c19b60ffd4bd3b075deb7520a720
wawerusamuel <waweruzamuel@gmail.com>
2022-09-21 10:43:13 +0300

    created a user data collecting form in order to meter the number of reaction a single user can make in a single blog...

---
+++ src/components/js/UserData.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import "../css/bootstrap.min.css";
+
+const UserData = () => {
+  const [name, setName] = useState(null);
+  const [email, setEmail] = useState(null);
+
+  return (
+    <form
+      className="form container"
+      onSubmit={(e) => {
+        SubmitData(e, name, email);
+      }}
+    >
+      <div className="form-group">
+        <input
+          type="text"
+          className="form-control"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Name"
+          required
+        />
+      </div>
+      <div className="form-group">
+        <input
+          type="email"
+          className="form-control"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+          required
+        />
+      </div>
+      <div className="form-group">
+        <button className="btn btn-outline-success">Submit</button>{" "}
+      </div>
+    </form>
+  );
+
+  function SubmitData(e, name, email) {
+    e.preventDefault();
+    const userData = { name, email };
+    let storage = JSON.parse(localStorage.getItem("blogs-user-data")) || {};
+    if (!storage) {
+      storage = userData;
+      localStorage.setItem("blogs-user-data", JSON.stringify(userData));
+    }
+  }
+};
+
+export default UserData;
