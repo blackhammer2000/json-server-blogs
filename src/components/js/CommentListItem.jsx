37b45dd525e178ff5132a78b2974ba3167bf2d4c
wawerusamuel <waweruzamuel@gmail.com>
2022-10-01 23:22:51 +0300

    added some styles to the comment list item to amek it a little clean...

--- src/components/js/CommentListItem.jsx
+++ src/components/js/CommentListItem.jsx
@@ -2,17 +2,15 @@ import React from "react";
 
 const CommentListItem = ({ comment }) => {
   return (
-    <div className="container d-flex flex-column border border-light mt-2 p-2">
-      <div className="container">
+    <div className="container d-flex flex-column border border-light mt-2 p-2 bg-dark">
+      <div className="container text-black-50">
         <h5>{comment.name}</h5>
       </div>
-      <div className="container">
+      <div className="container mt-1 text-white">
         <blockquote>{comment.body}</blockquote>
       </div>
-      <div className="container">
-        <h6>{comment.time_posted}</h6>
-      </div>
-      <div className="container-fluid comment_reactions row">
+
+      <div className="container-fluid comment_reactions row border-top px-0 pt-2 ml-1">
         <button className="btn btn-outline-primary border-light">
           <i className="fa fa-thumbs-up"></i>{" "}
           <span>
@@ -21,9 +19,12 @@ const CommentListItem = ({ comment }) => {
               : "Like"}
           </span>
         </button>
-        <button className="btn btn-outline-primary border-light ml-2">
+        <button className="btn btn-outline-primary border-light ml-3">
           <i className="fa fa-reply"></i> <span>Reply</span>
         </button>
+      </div>
+      <div className="container border-botttom d-flex justify-content-start align-items-center mt-2 px-2">
+        <h6>{comment.time_posted}</h6>
       </div>
     </div>
   );
