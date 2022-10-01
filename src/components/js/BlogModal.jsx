38580d1fd794593e941adf68cdffc30934639c8d
wawerusamuel <waweruzamuel@gmail.com>
2022-10-01 23:33:42 +0300

    creating a component for the edit blog functionality...

--- src/components/js/BlogModal.jsx
+++ src/components/js/BlogModal.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "../css/bootstrap.min.css";
 import CommentsList from "./AllComments";
+import EditBlog from "./EditBlog";
 // import { useHistory } from "react-router-dom";
 
 const BlogModal = ({
@@ -58,100 +59,14 @@ const BlogModal = ({
         </div>
       )}
       {blog && !error && blogToEditID && (
-        <form {...props} onSubmit={(e) => submitEdit(e)}>
-          <div className="container border-bottom pb-2 d-flex justify-content-around mt-3">
-            <h3>Edit Blog</h3>
-            <button
-              className="btn btn-outline-danger"
-              onClick={() => {
-                editBlog(null);
-                setSelectedBlog(null);
-              }}
-            >
-              X
-            </button>
-          </div>
-          <div className="form-group container">
-            <label htmlFor="">Title</label>
-            <input
-              type="text"
-              value={editTitle}
-              className="form-control"
-              onChange={(e) => updateState(e, setEditTitle)}
-              required
-            />
-          </div>
-          <div className="form-group container">
-            <label htmlFor="">Description</label>
-            <textarea
-              type="text"
-              value={editDescription}
-              className="form-control"
-              rows={6}
-              onChange={(e) => updateState(e, setEditDescription)}
-              required
-            />
-          </div>
-          <div className="form-group container">
-            <label htmlFor="">Author</label>
-            <input
-              type="text"
-              value={editAuthor}
-              className="form-control"
-              onChange={(e) => updateState(e, setEditAuthor)}
-              required
-            />
-          </div>
-          <div className="form-group container text-center mt-3">
-            <button
-              type="submit"
-              className="btn btn-outline-info w-75 text-center"
-            >
-              Edit blog
-            </button>
-          </div>
-        </form>
+        <EditBlog
+          blog={blog}
+          editTitle={editTitle}
+          setEditTitle={setEditTitle}
+        />
       )}
     </>
   );
-
-  function updateState(e, setter) {
-    setter(e.target.value);
-  }
-
-  async function submitEdit(e) {
-    e.preventDefault();
-    const newBlog = {
-      title: editTitle,
-      description: editDescription,
-      author: editAuthor,
-    };
-
-    const patchConfigurations = {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(newBlog),
-    };
-
-    try {
-      const res = await fetch(
-        `http://localhost:8000/blogs/${blog.id}`,
-        patchConfigurations
-      );
-      if (!res.ok) {
-        throw new Error("Server Error, Could Not Find Resources To Patch...");
-      }
-      console.log(`Blog ${blog.id} successfully edited...`);
-
-      window.location.pathname.includes("home")
-        ? window.location.pathname.replace("home", "")
-        : window.location.reload();
-    } catch (error) {
-      setError(error.message);
-    }
-  }
 };
 
 export default BlogModal;
---
+++ src/components/js/EditBlog.jsx
@@ -0,0 +1,96 @@
+import React from "react";
+
+const EditBlog = ({ blog }) => {
+  return (
+    <form {...props} onSubmit={(e) => submitEdit(e)}>
+      <div className="container border-bottom pb-2 d-flex justify-content-around mt-3">
+        <h3>Edit Blog</h3>
+        <button
+          className="btn btn-outline-danger"
+          onClick={() => {
+            editBlog(null);
+            setSelectedBlog(null);
+          }}
+        >
+          X
+        </button>
+      </div>
+      <div className="form-group container">
+        <label htmlFor="">Title</label>
+        <input
+          type="text"
+          value={editTitle}
+          className="form-control"
+          onChange={(e) => updateState(e, setEditTitle)}
+          required
+        />
+      </div>
+      <div className="form-group container">
+        <label htmlFor="">Description</label>
+        <textarea
+          type="text"
+          value={editDescription}
+          className="form-control"
+          rows={6}
+          onChange={(e) => updateState(e, setEditDescription)}
+          required
+        />
+      </div>
+      <div className="form-group container">
+        <label htmlFor="">Author</label>
+        <input
+          type="text"
+          value={editAuthor}
+          className="form-control"
+          onChange={(e) => updateState(e, setEditAuthor)}
+          required
+        />
+      </div>
+      <div className="form-group container text-center mt-3">
+        <button type="submit" className="btn btn-outline-info w-75 text-center">
+          Edit blog
+        </button>
+      </div>
+    </form>
+  );
+
+  function updateState(e, setter) {
+    setter(e.target.value);
+  }
+
+  async function submitEdit(e) {
+    e.preventDefault();
+    const newBlog = {
+      title: editTitle,
+      description: editDescription,
+      author: editAuthor,
+    };
+
+    const patchConfigurations = {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newBlog),
+    };
+
+    try {
+      const res = await fetch(
+        `http://localhost:8000/blogs/${blog.id}`,
+        patchConfigurations
+      );
+      if (!res.ok) {
+        throw new Error("Server Error, Could Not Find Resources To Patch...");
+      }
+      console.log(`Blog ${blog.id} successfully edited...`);
+
+      window.location.pathname.includes("home")
+        ? window.location.pathname.replace("home", "")
+        : window.location.reload();
+    } catch (error) {
+      setError(error.message);
+    }
+  }
+};
+
+export default EditBlog;
