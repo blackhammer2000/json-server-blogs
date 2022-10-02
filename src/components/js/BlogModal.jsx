7d8dbda673b3637cb41e97d6908c96109f656436
wawerusamuel <waweruzamuel@gmail.com>
2022-10-02 22:48:43 +0300

    refactored the blogmodal code...

--- src/components/js/BlogModal.jsx
+++ src/components/js/BlogModal.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "../css/bootstrap.min.css";
 import CommentsList from "./AllComments";
 import EditBlog from "./EditBlog";
