0944c96404c0412d41179f0450d96544d1309c1f
wawerusamuel <waweruzamuel@gmail.com>
2022-11-10 15:01:56 +0300

    created a.gitignore file and a .env file for the server...

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { connect } = require("mongoose");
+// const { connect } = require("mongoose");
 const { listen } = express();
 const port = 6000;
 
 listen(port, () => console.log(`server running on port ${port}.`));
 
-connect("mongodb://localhost/blogs", () => console.log("connected to db"));
+// const connection = await connect("mongodb://localhost/blogs");
+//  if(connection)console.log("connected to db")
