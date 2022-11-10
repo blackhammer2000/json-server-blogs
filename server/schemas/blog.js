8380b8de0a5f7f59c35f193ef3e02dfadff60a5c
wawerusamuel <waweruzamuel@gmail.com>
2022-11-10 18:47:19 +0300

    created the schema for the server...

---
+++ server/schemas/blog.js
@@ -0,0 +1,24 @@
+const { Schema, model } = require("mongoose");
+
+const blogSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+});
+
+const Blog = model("blog", blogSchema);
+
+module.exports = Blog;
