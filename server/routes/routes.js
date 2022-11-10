c71d39d07da5401ce843f00515b119b8a2526457
wawerusamuel <waweruzamuel@gmail.com>
2022-11-10 19:10:16 +0300

    created the route to get the blogs fromthe database...

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,4 +1,4 @@
-const { use } = require("express").Router();
+const router = require("express").Router();
 require("dotenv").config();
 
 const get_routes = require("./get_routes");
@@ -6,9 +6,9 @@ const post_routes = require("./post_routes");
 const patch_routes = require("./patch_routes");
 const delete_routes = require("./delete_routes");
 
-use(get_routes);
-use(post_routes);
-use(patch_routes);
-use(delete_routes);
+router.use(get_routes);
+router.use(post_routes);
+router.use(patch_routes);
+router.use(delete_routes);
 
 module.exports = router;
