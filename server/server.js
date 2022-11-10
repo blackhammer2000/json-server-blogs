db7b2c14270549a910b22ae6de71b7710c44b7d1
wawerusamuel <waweruzamuel@gmail.com>
2022-11-10 14:52:47 +0300

    created a connection with the local mongo db...

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const { connect } = require("mongoose");
 const { listen } = express();
 const port = 6000;
 
 listen(port, () => console.log(`server running on port ${port}.`));
+
+connect("mongodb://localhost/blogs", () => console.log("connected to db"));
