bb85eedf3df5af607de8a869c03c8b8cccd43c84
wawerusamuel <waweruzamuel@gmail.com>
2022-11-10 20:00:46 +0300

    destructured the json method from the express import statement...

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
 
-const express = require("express");
+const { json } = require("express");
 const routes = require("./routes/routes");
 const { connect } = require("mongoose");
 const { listen, use } = express();
 
-use(express.json());
+use(json());
 use(routes);
 
 const { PORT, DB_URL } = process.env;
