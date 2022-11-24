2352b7b45a7cde3fb7292715479255ce4c05d025
wawerusamuel <waweruzamuel@gmail.com>
2022-11-24 17:25:01 +0300

    created the like when creating a new blog...

--- server/routes/post_routes.js
+++ server/routes/post_routes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const Blog = require("../schemas/blog");
+const Like = require("../schemas/like");
 const { validateBlogBody } = require("./helpers/validators");
 
 router.post("/api/create/blog", validateBlogBody, async (req, res) => {
@@ -7,6 +8,10 @@ router.post("/api/create/blog", validateBlogBody, async (req, res) => {
     const blog = await Blog.create(req.body);
 
     if (!blog) throw new Error(blog);
+
+    const like = await Like.create({ blogID: blog._id, likes: [] });
+
+    if (!like) throw new Error(like);
 
     res.json({ message: "blog created", response_status: "success" });
   } catch (err) {
