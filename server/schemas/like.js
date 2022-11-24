f43efe74b6fc8c71cfa12731ae5f6c032f32b8b7
wawerusamuel <waweruzamuel@gmail.com>
2022-11-24 17:21:14 +0300

    updated the likes type from string to array...

--- server/schemas/like.js
+++ server/schemas/like.js
@@ -2,7 +2,7 @@ const { Schema, model } = require("mongoose");
 
 const likeSchema = new Schema({
   blogID: { type: String, required: true },
-  likes: { type: String, required: true },
+  likes: { type: Array, required: true },
 });
 
 const Like = model("like", likeSchema);
