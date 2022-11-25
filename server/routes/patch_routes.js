522cbd6080c76786ffba3c96c87fbe86bbf986e9
wawerusamuel <waweruzamuel@gmail.com>
2022-11-25 17:55:59 +0300

    corrected a typo in a response message...

--- server/routes/patch_routes.js
+++ server/routes/patch_routes.js
@@ -145,7 +145,7 @@ router.patch("/api/reactions/create/comments", async (req, res) => {
     if (!createComment) throw new Error(createComment);
 
     res.status(201).json({
-      message: `comment added successfully by user ${user._id}`,
+      message: `Comment added successfully by user ${user._id}`,
       comments,
     });
   } catch (err) {
