2fcd0c083e801909ea663b84dc82df87dd81cd2d
wawerusamuel <waweruzamuel@gmail.com>
2022-11-27 16:23:36 +0300

    working on the edit comment route...

--- server/routes/patch_routes.js
+++ server/routes/patch_routes.js
@@ -158,6 +158,28 @@ router.patch("/api/reactions/create/comment", async (req, res) => {
 
 router.patch("/api/reactions/edit/comment", async (req, res) => {
   try {
+    const { blogID, userID, commentID, commentReply } = req.body;
+
+    if (!blogID || !userID) throw new Error("Cannot proceed with the request.");
+
+    if (!userComment) throw new Error("No comment was submitted...");
+
+    const user = await User.findOne({ _id: userID });
+
+    if (!user) throw new Error("please sign up to react to any of the blogs.");
+
+    const blog = await Blog.findOne({ _id: blogID });
+
+    if (!blog) throw new Error(blog);
+
+    const comment = await Comment.findOne({ blogID: blog._id });
+
+    if (!comment) throw new Error(comment);
+
+    let { comments } = comment;
+
+    if (comments === (null || undefined))
+      throw new Error("error when fetching the blog likes.");
   } catch (err) {
     if (err.message) res.status(500).json({ error: err.message });
   }
