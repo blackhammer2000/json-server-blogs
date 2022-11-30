01047c077236afd3d3060043ffd01fd82782bab3
wawerusamuel <waweruzamuel@gmail.com>
2022-11-30 16:35:19 +0300

    created get routes to read a blogs reactions...

--- server/routes/get_routes.js
+++ server/routes/get_routes.js
@@ -1,5 +1,8 @@
 const router = require("express").Router();
 const Blog = require("../schemas/blog");
+const Comment = require("../schemas/comment");
+const Like = require("../schemas/like");
+const Share = require("../schemas/share");
 
 router.get("/api/read/blogs", async (req, res) => {
   try {
@@ -8,6 +11,42 @@ router.get("/api/read/blogs", async (req, res) => {
     if (!blogs) throw new Error(blogs);
 
     res.json({ blogs });
+  } catch (err) {
+    res.json({ error: err.message });
+  }
+});
+
+router.get("/api/blog/reactions/read/likes", async (req, res) => {
+  try {
+    const blogs = await Like.findOne({ blogID: req.body.blogID });
+
+    if (!blogs) throw new Error(blogs);
+
+    res.json({ blogLikes: blogs.likes.length });
+  } catch (err) {
+    res.json({ error: err.message });
+  }
+});
+
+router.get("/api/blog/reactions/read/comments", async (req, res) => {
+  try {
+    const blogs = await Comment.findOne({ blogID: req.body.blogID });
+
+    if (!blogs) throw new Error(blogs);
+
+    res.json({ blogLikes: blogs.comments.length });
+  } catch (err) {
+    res.json({ error: err.message });
+  }
+});
+
+router.get("/api/blog/reactions/read/shares", async (req, res) => {
+  try {
+    const blogs = await Share.findOne({ blogID: req.body.blogID });
+
+    if (!blogs) throw new Error(blogs);
+
+    res.json({ blogLikes: blogs.shares.length });
   } catch (err) {
     res.json({ error: err.message });
   }
