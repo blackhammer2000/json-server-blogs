a2439dc81a7acc5c0294171d059fbc11d517aaec
wawerusamuel <waweruzamuel@gmail.com>
2022-11-30 15:28:58 +0300

    added the cors moduke to the server...

--- server/server.js
+++ server/server.js
@@ -1,10 +1,16 @@
 require("dotenv").config();
 
 const express = require("express");
+const cors = require("cors");
 const routes = require("./routes/routes");
 const { connect } = require("mongoose");
 const app = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 app.use(express.json());
 app.use(routes);
 
