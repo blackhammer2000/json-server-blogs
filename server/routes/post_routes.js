de46dfaf55ff46388d566a1ef2a7d052bbbd0b0f
wawerusamuel <waweruzamuel@gmail.com>
2022-12-06 20:33:42 +0300

    added a more descriptive error message...

--- server/routes/post_routes.js
+++ server/routes/post_routes.js
@@ -72,7 +72,7 @@ router.post("/api/blog/reactions/read/likes", async (req, res) => {
   try {
     const blog = await Like.findOne({ blogID: req.body.blogID });
 
-    if (!blog) throw new Error(blogs);
+    if (!blog) throw new Error("No blog withthe given ID was found.");
 
     res.json({ likes: blogs.likes.length });
   } catch (err) {
